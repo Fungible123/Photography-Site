8ff30906afb1f2b0f590ca6d67b31598bc800d4f
Donny Layug <donn.layug@yahoo.com>
2019-04-16 17:49:20 +0800

    Finished creating and styling all the other links of the site. Will need to finalize things and make documentation in github.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 // When the camera button is clicked
 
-const cameraBtn = () => {
+const cameraBtn = function() {
     const camera = document.querySelector('.toggle-button');
     const navbar = document.querySelector('.mobile-nav');
 
-    camera.addEventListener('click', (e) => {
+    camera.addEventListener('click', function(e) {
         e.preventDefault();
         navbar.classList.toggle('open');
     });
@@ -48,5 +48,14 @@ ScrollReveal().reveal('.image7', { delay: 1400, easing: 'ease-in', origin: 'righ
 ScrollReveal().reveal('.image8', { delay: 1500, easing: 'ease-in', origin: 'right' });
 ScrollReveal().reveal('.image9', { delay: 1600, easing: 'ease-in', origin: 'right' });
 ScrollReveal().reveal('.image10', { origin: 'right' } );
-ScrollReveal().reveal('.image11', { delay: 300, origin: 'right' });
-ScrollReveal().reveal('.image12', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.image11', { delay: 300, easing: 'ease-in', origin: 'right' });
+ScrollReveal().reveal('.image12', { delay: 500, easing: 'ease-in', origin: 'right' });
+ScrollReveal().reveal('.image13', { easing: 'ease-in', origin: 'bottom' });
+ScrollReveal().reveal('.image14', { delay: 500, easing: 'ease-in', origin: 'bottom' });
+ScrollReveal().reveal('.image15', { delay: 500, easing: 'ease-in', origin: 'bottom' } );
+ScrollReveal().reveal('.image16', { delay: 1000, easing: 'ease-in', origin: 'bottom' });
+ScrollReveal().reveal('.image17', { easing: 'ease-in', origin: 'bottom' });
+ScrollReveal().reveal('.text-scroll', { delay: 500, easing: 'ease-in', origin: 'bottom' });
+ScrollReveal().reveal('.image18', { delay: 1500, easing: 'ease-in', origin: 'bottom' } );
+ScrollReveal().reveal('.image19', { delay: 2000, easing: 'ease-in', origin: 'bottom' } );
+ScrollReveal().reveal('.about-content', { delay: 1500, easing: 'ease-in', origin: 'bottom' } );
